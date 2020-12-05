01dfdc2025e1e38d5b4b6cffe173b42a2bbf7d6c
Mai Houa Vue <mhvue4@gmail.com>
2020-12-05 13:13:00 -0600

    added js challenge number 5 - reverse a string

---
+++ challenges/challenge5.js
@@ -0,0 +1,8 @@
+//reverse a string 
+
+function reverseAString(str) {
+    //  make str to an array first
+    //reverve method (which only works on arrays)
+    //then take array of str back to a str 
+    return str.split("").reverse().join("");
+}
