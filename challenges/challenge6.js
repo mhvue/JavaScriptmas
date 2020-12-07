266064f919f2e43e4be6c23cf2d18fe1ec0d95cc
Mai Houa Vue <mhvue4@gmail.com>
2020-12-06 21:33:59 -0600

    added in challenge 6 - order elements in array by length.

---
+++ challenges/challenge6.js
@@ -0,0 +1,19 @@
+//sort element in array by length, if same length, return those items it the order of the array in your answer
+//https://scrimba.com/learn/adventcalendar/cyw87yuK?utm_source=newsletter&utm_medium=email&utm_campaign=javascriptmas_day6&purge=true
+
+function sortByLength(strs) {
+    //  sort by increasing lengths
+    // if same length, must be same order as original array
+
+    //sort method ascending
+   let sorted = strs.sort(function(a,b){
+        return a.length - b.length;
+        
+        if(a.length === b.length){
+            return b.length - a.length;
+    
+        }
+    });
+
+return sorted;
+}
