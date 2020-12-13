7320246ff9a5ba60d11021f6f89d6835102af93a
Mai Houa Vue <mhvue4@gmail.com>
2020-12-12 20:43:38 -0600

    added challenge 11 solution. missed it and unable to get solution in time.

---
+++ challenges/challenge11.js
@@ -0,0 +1,12 @@
+//did not get this challenge. here is solution per scrimba:
+//https://scrimba.com/learn/adventcalendar/-javascript-challenge-avoid-obstacles-solution-czbw6dS3
+
+// function avoidObstacles(nums) {
+//     const largestNum = nums.sort((a, b) => a-b)[nums.length - 1];
+    
+//     for (let i = 1; i <= largestNum + 1; i++) {
+//         if(nums.every((value) => value % i !== 0)) {
+//             return i;
+//         } 
+//     }
+// }
