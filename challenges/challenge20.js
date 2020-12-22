f3499ac91ff758f9b9188ccfa38c00dbaba270b1
Mai Houa Vue <mhvue4@gmail.com>
2020-12-21 19:49:08 -0600

    updated solution to challenge 20 based on solution

--- challenges/challenge20.js
+++ challenges/challenge20.js
@@ -4,7 +4,6 @@ function domainType(domains) {
     //.org = organization
     //.net = network
     //.info = information
-    const domainInfo = ["com", "org", "net", "info"];
     const newArr = [];
     //can check if arr contains the string domainInfo
     
@@ -15,37 +14,27 @@ function domainType(domains) {
         
         //break up each domain by the period
         const split = eachDomain.split(".")
-       // console.log(split)
+      // console.log(split.length)
         
-        //loop through those split up
-        for (let j = 0; j < split.length; j++){
-            //console.log(split[j])
-            
-            //check to see if each match the domainInfo array
-            for(let k = 0; k < domainInfo.length; k++){
-                
-                //write out what each domainInfo stands for and push that to newArr
-                if(split[j] === domainInfo[k]){
-                    if(domainInfo[k] === "org"){
-                        newArr.push("organization")
-                    }
-                    else if(domainInfo[k] === "net"){
-                        newArr.push("network")
-                    }
-                    else if(domainInfo[k] === "com"){
-                        newArr.push("commercial")
-                    }
-                    else{
-                        newArr.push("information")
-                    }
-                }
-        
-            }
+       const domainOnly = split[split.length-1] //get only the last val at that last index 
+      
+    
+        if(domainOnly === "org"){
+            newArr.push("organization")
         }
+        else if(domainOnly === "net"){
+            newArr.push("network")
+        }
+        else if(domainOnly === "com"){
+            newArr.push("commercial")
+        }
+        else{
+            newArr.push("information")
+                    }
     }
    
     return newArr;
 }
 
 
-//i do not like my soluation above as it contains 3 nested loops. need to refractor 
+//refactor after waching solution 12/21/20
