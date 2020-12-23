82d5831e436bffe22afb55f8069a98d6ea7ee7f7
Mai Houa Vue <mhvue4@gmail.com>
2020-12-22 19:51:42 -0600

    added js challenge 21 solution by scrimba.

---
+++ challenges/challenge21.js
@@ -0,0 +1,21 @@
+// given two arrays and value, find if the numbers in array can add up to value 
+//https://scrimba.com/learn/adventcalendar/-javascript-challenge-sum-of-two-solution-cgKaKycr
+//I was unable to solve. 
+
+//solution per scrimba:
+// function sumOfTwo(nums1, nums2, value) {
+//     const map = {};
+    
+//     for(let num of nums1) {
+//         const difference = value - num;
+//         map[difference] = difference;
+//     }
+    
+//     for(let num of nums2) {
+//         if(map.hasOwnProperty(num)) {
+//             return true;
+//         }
+//     }
+    
+//     return false;
+// }
