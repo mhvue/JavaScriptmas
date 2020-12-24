43ce44ed1cf649b7e9e369f57fb0af40c9e7eebe
Mai Houa Vue <mhvue4@gmail.com>
2020-12-23 18:03:53 -0600

    added js challenge 23 - social media text input count

---
+++ challenges/challenge23.js
@@ -0,0 +1,33 @@
+// DESCRIPTION:
+// We are making a Social Media Character Counter! We want to display the available characters LEFT.
+// Using the Keydown event should help you here. When the characters reach 20 and below, we want them to turn red. So we will use Javascript to add that styling to it. If the characters drop below 0, we want the button to be disabled BUT if there are only 0 characters left, we should still be able to tweet.
+//https://scrimba.com/learn/adventcalendar/-javascript-challenge-social-media-input-crqLDVud
+
+let count = 140;
+let countHolder;
+
+//event listener is key down
+document.onkeydown = function(event){
+    const letter = event.key;
+
+    
+    //update counterfooter here
+    countHolder = count--;
+   document.getElementById("counterFooter").innerHTML = countHolder +"/" +140;
+    
+    //if pressed delete or backspace, increase count instead 
+    if(event.keyCode == 8 || event.keyCode == 46){
+        countHolder = count++;
+        document.getElementById("counterFooter").innerHTML = countHolder +"/" +140;
+    }
+    //each 20 and below, we want them to turn red.
+    else if(count <= 20){
+        document.getElementById("string").style.color = "red";
+    }
+   // drop below 0, we want the button to be disabled
+   else if(count < 0){
+       //add class name to make button disabled ...now working yet
+       document.getElementById("btn").className = "buttonDisabled";
+   }
+   
+}
